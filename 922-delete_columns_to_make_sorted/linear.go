467b8367497f561aac01c80957434190f90346bc
Phea Duch <pd@fromio.com>
2020-09-08 18:34:47 -0400

    easy: 922-Delete Columns to make Sorted

---
+++ 922-delete_columns_to_make_sorted/linear.go
@@ -0,0 +1,22 @@
+package lc
+
+// Time: O(n) = n is number of characters
+// Benchmark: 8ms 6.1mb | 93%
+
+func minDeletionSize(A []string) int {
+	if len(A) <= 1 {
+		return 0
+	}
+
+	var count int
+	// sort.Strings(A)
+	for i := range A[0] {
+		for j := 0; j <= len(A)-1; j++ {
+			if A[j][i] >= A[j+1][i] 
+				count++
+				break
+			}
+		}
+	}
+
+	return count
