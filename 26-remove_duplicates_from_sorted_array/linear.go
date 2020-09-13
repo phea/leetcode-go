4486ec3816c0ba586c9ff1076e7008eb8218f646
Phea Duch <pd@fromio.com>
2020-09-13 09:00:37 -0400

    fixed function name

--- 26-remove_duplicates_from_sorted_array/linear.go
+++ 26-remove_duplicates_from_sorted_array/linear.go
@@ -4,7 +4,7 @@ package lc
 // Benchmark: 32ms 4.6 | 7% 20%
 
 // We remove all duplicates in a single append.
-func removeDuplicatesA(nums []int) int {
+func removeDuplicates(nums []int) int {
 	for i := 0; i < len(nums)-1; i++ {
 		if nums[i] == nums[i+1] {
 			j := i + 1
