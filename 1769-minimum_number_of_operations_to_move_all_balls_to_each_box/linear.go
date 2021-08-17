cf046aeef685a336c2e12449663685a2b2517174
Phea Duch <pd@fromio.com>
2021-08-17 11:16:56 -0400

    removed unnecessary variable

--- 1769-minimum_number_of_operations_to_move_all_balls_to_each_box/linear.go
+++ 1769-minimum_number_of_operations_to_move_all_balls_to_each_box/linear.go
@@ -6,15 +6,13 @@ package lc
 func minOperations2(boxes string) []int {
 	var ln, rn int
 	resp := make([]int, len(boxes))
-	var d int
 	for i, ch := range boxes {
 		if ch == '1' {
-			d += i
+			resp[0] += i
 			rn++
 		}
 	}
 
-	resp[0] = d
 	for i := 1; i < len(resp); i++ {
 		if boxes[i-1] == '1' {
 			rn--
